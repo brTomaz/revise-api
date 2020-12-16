8716e9b9d826a5be97028f31f98c6d9432700700
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-12-16 20:08:01 -0300

    refactor: change database configs

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'revise_host'),
-        port: env.int('DATABASE_PORT', 1111),
-        database: env('DATABASE_NAME', 'revisedb'),
-        username: env('DATABASE_USERNAME', 'revise_user'),
-        password: env('DATABASE_PASSWORD', 'revise_password'),
+        host: env('DATABASE_HOST', ''),
+        port: env.int('DATABASE_PORT', 1),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
