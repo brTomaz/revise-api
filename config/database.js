a0bcf6aedbedb357f0bd372641bda1d1ff1674f0
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-12-16 21:24:31 -0300

    refactor: change database configs

--- config/database.js
+++ config/database.js
@@ -12,7 +12,9 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {}
+      options: {
+        ssl: false
+      }
     },
   },
 });
